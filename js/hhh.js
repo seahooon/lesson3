b5d9f99c8a6b08351860b3f923b0c62fae323f90
seahooon <nastya.2345446@gmail.com>
2019-02-16 13:59:32 +0200

    end of lesson3

---
+++ js/hhh.js
@@ -0,0 +1,31 @@
+//var array =[];//пустой масив, ковычки - строка
+//var array = [true, "Cat", "777", []];
+
+
+// var array = ["пушок", "барсик", "рыжик"];
+// for (var i = 0; i < array.length; i++) {
+// 	console.log(array[i])
+// }
+
+// var array = ["пушок", "барсик", "рыжик"];
+// for (var i = array.length - 1; i = 0; i--) {
+//  console.log(array[i])
+// }
+
+//console.log(array);
+
+// var array = ["пушок", "барсик", "рыжик"];
+// for (var i = 0; i < array.length; i++) {
+// 	if (array[i] = "барсик"){
+// 		console.log(`У меня есть ${array[i]} Мурка`)
+// 	}
+// 	if (array[i] = "пушок"){
+// 		console.log(`У меня есть ${array[i]}`) ;
+// 	}
+
+
+var array = ["кот", "кошка", "белка"];
+array.pop();//удаляет последний елемент массива
+console.log(array);
+array.push("кит", "слон", "яблоко", 77, true, ["String", "String2"]);//добавляет последний елемент массива
+console.log(array[7][1]);
